2c2b3e83a225c44577ceac1cd304d4bdfdfa1a63
Andressa <andressavitoria.rsantos@gmail.com>
2023-04-07 11:38:38 -0300

    Aula 2 completa

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const robotron = document.querySelector("#robotron");
+
+function dizOi() {
+    console.log('Oii, bem-vindo');
+}
+
+robotron.addEventListener("click", () => {
+    console.log('Cliquei no robo')
+});
+
+dizOi();
